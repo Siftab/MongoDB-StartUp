a1876807c531651fe2ee173e62128f39148d72bc
Siftab <siiffuuu@gmail.com>
2023-12-16 12:50:18 +0600

    final overview

--- simple-crud-server/__Steps.js
+++ simple-crud-server/__Steps.js
@@ -37,10 +37,32 @@
  * 1. create app.delete.('/user/:id',async(req,res)=>{})
  * 2.specify unique ObjectID to delete the right user 
  * 3. const query ={_id: new ObjectId(id) }
- * 4. const reult = await userCollection.deleteOne(query );
+ * 4. const result = await userCollection.deleteOne(query );
  * 
  * Client
  * --
  * 1. create dynamic url with id 
  * 2. mention the DELETE method 
+ * _____________________________________________________
+ * UPDATE
+ * -----------------
+ * 1.app.put('/users/:id',async(req,res)=>{
+ *                                          })
+ * 2.const id = req.params.id  eta diye id ta nite hobe 
+ * 3.const user = req.body eta diye updated user data nite hobe 
+ * then Mongo code 
+ * 4. const filter= {_id:new ObjectId(id)} similar like queary we have used in delete and 
+ * 5.const options= { upsert:true}  // upsert=> update + insert it allows both 
+ * 6.then make a object variable with $set && const updateUser= {
+        $set:{
+           name: user.name,
+           email: user.email
+        }
+      }
+ * 7.then  const result =await userCollection.updateOne(filter,updateUser,    options)  || use filter , updateUSer and option sequentially and send result as response 
+ * 
+ * ---------------------------
+ *client side
+ *------------------------------
+ * as as usal fetch kkore server k hit korbo and metho must be "PUT" Likhte hobe baki ja ache same ager motoi 
  * /
